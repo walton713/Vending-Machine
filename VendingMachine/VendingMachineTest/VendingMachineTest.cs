0d414f58df40002c269d2b9ffbd8427ac270af9a
Ben Walton <waltonb@oregonstate.edu>
2017-09-11 08:31:22 -0500

    Refactored tests
    Refactored tests

--- VendingMachine/VendingMachineTest/VendingMachineTest.cs
+++ VendingMachine/VendingMachineTest/VendingMachineTest.cs
@@ -8,10 +8,17 @@ namespace VendingMachineTest
     [TestClass]
     public class VendingMachineTest
     {
+        VendingMachine vendingMachine;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            vendingMachine = new VendingMachine();
+        }
+
         [TestMethod]
         public void WhenANickelIsInsertedItIsAccepted()
         {
-            VendingMachine vendingMachine = new VendingMachine();
             vendingMachine.Insert(VendingMachine.Coins.NICKEL);
             Assert.AreEqual(VendingMachine.Coins.NICKEL, vendingMachine.CurrentCoins[0]);
         }
@@ -19,7 +26,6 @@ namespace VendingMachineTest
         [TestMethod]
         public void WhenADimeIsInsertedItIsAccepted()
         {
-            VendingMachine vendingMachine = new VendingMachine();
             vendingMachine.Insert(VendingMachine.Coins.DIME);
             Assert.AreEqual(VendingMachine.Coins.DIME, vendingMachine.CurrentCoins[0]);
         }
@@ -27,7 +33,6 @@ namespace VendingMachineTest
         [TestMethod]
         public void WhenAQuarterIsInsertedItIsAccepted()
         {
-            VendingMachine vendingMachine = new VendingMachine();
             vendingMachine.Insert(VendingMachine.Coins.QUARTER);
             Assert.AreEqual(VendingMachine.Coins.QUARTER, vendingMachine.CurrentCoins[0]);
         }
