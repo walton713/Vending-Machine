0dc0ac144f7d97343e8c6e57484df6ef8256747c
Ben Walton <waltonb@oregonstate.edu>
2017-09-11 08:45:28 -0500

    Test 4 green
    Insert penny test is green

--- VendingMachine/VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine/VendingMachine.cs
@@ -9,18 +9,45 @@ namespace Machine
 {
     public class VendingMachine
     {
-        public enum Coins { NICKEL, DIME, QUARTER };
+        public enum Coins { NICKEL, DIME, QUARTER, PENNY };
 
         public ArrayList CurrentCoins {get; private set;}
+        public ArrayList CoinReturn { get; private set; }
+        public double CurrentAmount { get; private set; }
+        public string Display { get; private set; }
 
         public VendingMachine()
         {
             CurrentCoins = new ArrayList();
+            CoinReturn = new ArrayList();
+            CurrentAmount = 0;
+            Display = "INSERT COIN";
         }
 
         public void Insert(Coins coin)
         {
-            CurrentCoins.Add(coin);
+            if (coin == Coins.PENNY)
+            {
+                CoinReturn.Add(coin);
+                Display = "INSERT COIN";
+            }
+            else
+            {
+                CurrentCoins.Add(coin);
+                if (coin == Coins.NICKEL)
+                {
+                    CurrentAmount += 0.05;
+                }
+                else if (coin == Coins.DIME)
+                {
+                    CurrentAmount += 0.10;
+                }
+                else if (coin == Coins.QUARTER)
+                {
+                    CurrentAmount += 0.25;
+                }
+                Display = string.Format("{0:0.00}", CurrentAmount);
+            }
         }
 
         static void Main(string[] args)
--- VendingMachine/VendingMachineTest/VendingMachineTest.cs
+++ VendingMachine/VendingMachineTest/VendingMachineTest.cs
@@ -21,6 +21,8 @@ namespace VendingMachineTest
         {
             vendingMachine.Insert(VendingMachine.Coins.NICKEL);
             Assert.AreEqual(VendingMachine.Coins.NICKEL, vendingMachine.CurrentCoins[0]);
+            Assert.AreEqual(0.05, vendingMachine.CurrentAmount);
+            Assert.AreEqual("0.05", vendingMachine.Display);
         }
 
         [TestMethod]
@@ -28,6 +30,8 @@ namespace VendingMachineTest
         {
             vendingMachine.Insert(VendingMachine.Coins.DIME);
             Assert.AreEqual(VendingMachine.Coins.DIME, vendingMachine.CurrentCoins[0]);
+            Assert.AreEqual(0.10, vendingMachine.CurrentAmount);
+            Assert.AreEqual("0.10", vendingMachine.Display);
         }
 
         [TestMethod]
@@ -35,6 +39,15 @@ namespace VendingMachineTest
         {
             vendingMachine.Insert(VendingMachine.Coins.QUARTER);
             Assert.AreEqual(VendingMachine.Coins.QUARTER, vendingMachine.CurrentCoins[0]);
+            Assert.AreEqual(0.25, vendingMachine.CurrentAmount);
+            Assert.AreEqual("0.25", vendingMachine.Display);
+        }
+
+        [TestMethod]
+        public void WhenAPennyIsInsertedItIsRejected()
+        {
+            vendingMachine.Insert(VendingMachine.Coins.PENNY);
+            Assert.AreEqual(VendingMachine.Coins.PENNY, vendingMachine.CoinReturn[0]);
         }
     }
 }
