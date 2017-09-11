4193c89f1deceaccbff7e24def7d7ff7e142bebd
Ben Walton <waltonb@oregonstate.edu>
2017-09-11 08:14:26 -0500

    First Test Green
    Insert nickel test is green.

---
+++ VendingMachine/VendingMachine/VendingMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine
+{
+    public class VendingMachine
+    {
+        public enum Coins { NICKEL };
+
+        public ArrayList CurrentCoins {get; private set;}
+
+        public VendingMachine()
+        {
+            CurrentCoins = new ArrayList();
+        }
+
+        public void Insert(Coins coin)
+        {
+            CurrentCoins.Add(coin);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ VendingMachine/VendingMachineTest/VendingMachineTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Machine;
+
+namespace VendingMachineTest
+{
+    [TestClass]
+    public class VendingMachineTest
+    {
+        [TestMethod]
+        public void WhenANickelIsInsertedItIsAccepted()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            vendingMachine.Insert(VendingMachine.Coins.NICKEL);
+            Assert.AreEqual(VendingMachine.Coins.NICKEL, vendingMachine.CurrentCoins[0]);
+        }
+    }
+}
